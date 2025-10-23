b2bfcfc158f8bc133565d8886020d172f7237630
lazarevFedor <lazarevfedor3@gmail.com>
2025-10-23 22:49:49 +0300

    WISETASK-5 add: exception classes

---
+++ llm-service/app/core/exceptions.py
@@ -0,0 +1,34 @@
+class LLMClientError(Exception):
+    """Base LLM Client Error"""
+    pass
+
+
+class LLMTimeoutError(LLMClientError):
+    """Timeout Error while connecting to LLM"""
+    def __init__(self, operation: str, timeout_seconds: float):
+        self.operation = operation
+        self.timeout_seconds = timeout_seconds
+        message = f'Operation {operation} timed out after {timeout_seconds} seconds.'
+        super().__init__(message)
+
+
+class LLMUnavailableError(LLMClientError):
+    """LLM Unavailable Error"""
+    def __init__(self, url: str, error_description: str):
+        self.url = url
+        self.error_description = error_description
+        message = f'Operation {url} unavailable at {error_description}.'
+        super().__init__(message)
+
+
+class LLMOverloadError(LLMClientError):
+    """LLM Mover Load Error"""
+    def __init__(self, max_concurrent_requests: int):
+        self.max_concurrent_requests = max_concurrent_requests
+        message = f'Reached maximum concurrent requests: {max_concurrent_requests}.'
+        super().__init__(message)
+
+
+class LLMGenerationError(LLMClientError):
+    """Error while generating answer"""
+    pass
