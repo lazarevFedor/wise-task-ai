e68c7af9ff29287433754eae8b6d4cbc90df0146
lazarevFedor <lazarevfedor3@gmail.com>
2025-10-30 22:51:28 +0300

    WISETASK-5 add: PromptEngine class template

---
+++ llm-service/app/core/promt_engine.py
@@ -0,0 +1,16 @@
+class PromptEngine:
+    def __init__(self):
+        pass
+
+    def _load_templates(self):
+        pass
+
+
+    def build_promt(self):
+        pass
+
+    def _get_definition_template(self):
+        pass
+
+    def _get_explanation_template(self):
+        pass
