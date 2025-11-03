c710e34d7e51639159f1a4a5270c26b5401ce202
lazarevFedor <lazarevfedor3@gmail.com>
2025-11-03 19:16:36 +0300

    WISETASK-5 add: proto-gen files

---
+++ llm-service/app/core/gen/llm_service/llm_service_pb2.pyi
@@ -0,0 +1,41 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GenerateRequest(_message.Message):
+    __slots__ = ("question", "contexts")
+    QUESTION_FIELD_NUMBER: _ClassVar[int]
+    CONTEXTS_FIELD_NUMBER: _ClassVar[int]
+    question: str
+    contexts: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, question: _Optional[str] = ..., contexts: _Optional[_Iterable[str]] = ...) -> None: ...
+
+class GenerateResponse(_message.Message):
+    __slots__ = ("answer", "processingTime", "errorMessage", "success")
+    ANSWER_FIELD_NUMBER: _ClassVar[int]
+    PROCESSINGTIME_FIELD_NUMBER: _ClassVar[int]
+    ERRORMESSAGE_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    answer: str
+    processingTime: float
+    errorMessage: str
+    success: bool
+    def __init__(self, answer: _Optional[str] = ..., processingTime: _Optional[float] = ..., errorMessage: _Optional[str] = ..., success: bool = ...) -> None: ...
+
+class HealthRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class HealthResponse(_message.Message):
+    __slots__ = ("healthy", "status_message", "model_loaded")
+    HEALTHY_FIELD_NUMBER: _ClassVar[int]
+    STATUS_MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    MODEL_LOADED_FIELD_NUMBER: _ClassVar[int]
+    healthy: bool
+    status_message: str
+    model_loaded: str
+    def __init__(self, healthy: bool = ..., status_message: _Optional[str] = ..., model_loaded: _Optional[str] = ...) -> None: ...
