48011b1a7ef29316dda3071746aba8f585d9b8c2
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-05 23:18:58 +0300

    fix(server): some small mistakes

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,14 +1,26 @@
 package main
 
-import(
+import (
 	"context"
+	"fmt"
 	"github.com/lazarevFedor/wise-task-ai/pkg/logger"
+	pb "github.com/lazarevFedor/wise-task-ai/pkg/server_api"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 )
 
-
-func main(){
-	
+func main() {
 	ctx := context.Background()
-	log := logger.NewLogger(ctx)
+	log, err := logger.NewLogger(ctx)
+	if err != nil {
+
+	}
+	container, Port := "name", "0000"
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%s", container, Port), grpc.Withinsecure())
+	if err != nil {
+		log.Error(ctx, "failed to connect to llm service", zap.Error(err))
+		return
+	}
+	defer conn.Close()
 
 }
--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -1,10 +1,16 @@
 package server
 
 import(
-	"github.com/lazarevFedor/wise-task-ai/pkg/api"
+	"github.com/lazarevFedor/wise-task-ai/server/pkg/api"
+	"context"
+	"github.com/lazarevFedor/wise-task-ai/server/pkg/logger"
 )
 
 type Server struct{
 	api.UnimplementedAIServiceServer
+		
+}
+
+func (s *Server) Prompt(ctx context.Context, req *api.PromptRequest) (*api.PromptResponse, error){
 	
 }
