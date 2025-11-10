568236237f23b7139db0499c5cea6d965ea7a583
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-10 13:33:44 +0300

    feat(core): some modifications
    - main.go:
        add config
    - server.go:
        add server functionality

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,15 +4,16 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/lazarevFedor/wise-task-ai/server/internal/server"
-	api "github.com/lazarevFedor/wise-task-ai/server/pkg/api/core-service"
+	"github.com/lazarevFedor/wise-task-ai/server/internal/config"
+	"github.com/lazarevFedor/wise-task-ai/server/internal/coreserver"
+	"github.com/lazarevFedor/wise-task-ai/server/pkg/api/core-service"
+	"github.com/lazarevFedor/wise-task-ai/server/pkg/api/llm-service"
 	"github.com/lazarevFedor/wise-task-ai/server/pkg/logger"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
-
 
 func main() {
 	ctx := context.Background()
@@ -22,22 +23,25 @@ func main() {
 		return
 	}
 
-	container, port := "localhost", "0000"
-	clientConn, err := grpc.NewClient(fmt.Sprintf("%s:%s", container, port), 
-										grpc.WithTransportCredentials(insecure.NewCredentials()))
+	cfg, err := config.NewCoreServerConfig()
 
-	if err != nil{
+	container, port := "localhost", "0000"
+	clientConn, err := grpc.NewClient(fmt.Sprintf("%s:%s", container, port),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
 		log.Error(ctx, "failed to connect to LLM Service", zap.Error(err))
 		return
 	}
 
-	client := api.NewCoreServiceClient(clientConn)
+	client := llm.NewLlmServiceClient(clientConn)
 
-	server2LLM := server.NewServer(client)
+	server2LLM, err := coreserver.NewServer(ctx, client, cfg)
 
+	//TODO: add arguments to grpcc.NewServer func
 	server := grpc.NewServer()
 
-	api.RegisterCoreServiceServer(server, server2LLM)
+	core.RegisterCoreServiceServer(server, server2LLM)
 	reflection.Register(server)
 
 	// DB
