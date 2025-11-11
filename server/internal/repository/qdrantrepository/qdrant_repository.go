51ad39b4aead2178cf47b07d648e6705645b2883
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-11 03:04:25 +0300

    feat(Qdrant): edit Qdrant repository layer - edit return value - parse search result

--- server/internal/repository/qdrantrepository/qdrant_repository.go
+++ server/internal/repository/qdrantrepository/qdrant_repository.go
@@ -24,13 +24,23 @@ func NewRepository(client *qdrant.Client) *QdrantRepository {
 	return &QdrantRepository{client}
 }
 
-func (repo *QdrantRepository) Search(ctx context.Context, requestVector []float32) ([]*qdrant.ScoredPoint, error) {
-	result, err := repo.QdrantClient.Query(ctx, &qdrant.QueryPoints{
+func (repo *QdrantRepository) Search(ctx context.Context, requestVector []float32) ([]string, error) {
+	queryResponce, err := repo.QdrantClient.Query(ctx, &qdrant.QueryPoints{
 		CollectionName: collection,
 		Query:          qdrant.NewQuery(requestVector...),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("Search: failed to search in Qdrant: %w", err)
+	}
+	result := make([]string, 0)
+
+	//TODO: result[0].Payload["Text"].GetStringValue()
+	//TODO: result[0].String()
+	for i, point := range queryResponce {
+		result = append(result, point.String())
+		if i == 3 {
+			break
+		}
 	}
 	return result, nil
 }
