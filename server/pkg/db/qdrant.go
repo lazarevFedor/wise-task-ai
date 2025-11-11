d62df37c2589176be66890a85ead1b807e159d6f
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-11 03:07:42 +0300

    fix(Qdrant): edit Qdrant client - Изменены теги конфига - изменен параметр функции NewQdrant (cfg)

--- server/pkg/db/qdrant.go
+++ server/pkg/db/qdrant.go
@@ -9,21 +9,20 @@ import (
 )
 
 type QdrantConfig struct {
-	Host string `env:"CORE_QDRANT_HOST"`
-	Port int    `env:"CORE_QDRANT_PORT"`
+	Host string `env:"HOST"`
+	Port int    `env:"PORT"`
 }
 
-func NewQdrant(ctx context.Context, cfg *QdrantConfig) (*qdrant.Client, error) {
+func NewQdrant(ctx context.Context, cfg QdrantConfig) (*qdrant.Client, error) {
 	client, err := qdrant.NewClient(&qdrant.Config{
 		Host: cfg.Host,
 		Port: cfg.Port,
 	})
 	if err != nil {
-		return nil, fmt.Errorf("NewCoreReository: failed to connect to qdrant: %w", err)
+		return nil, fmt.Errorf("NewCoreRepository: failed to connect to Qdrant: %w", err)
 	}
 
 	log := logger.GetLoggerFromCtx(ctx)
-	log.Info(ctx, "connected to qdrant db")
-
+	log.Info(ctx, "connected to Qdrant db")
 	return client, nil
 }
