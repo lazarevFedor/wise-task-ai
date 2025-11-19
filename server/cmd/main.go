2bc1012baa856c1b98db34b5905bf354574c3ff2
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-19 20:07:37 +0300

    fix(core): remove consts, change ctx to rootCtx, add extra logging

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"net"
 
+	
 	"github.com/lazarevFedor/wise-task-ai/server/internal/config"
+	"github.com/lazarevFedor/wise-task-ai/server/internal/interceptors"
 	"github.com/lazarevFedor/wise-task-ai/server/internal/coreserver"
 
 	"github.com/lazarevFedor/wise-task-ai/server/pkg/api/core-service"
@@ -18,33 +20,29 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-const (
-	llmHost = "llm-service"
-	llmPort = "8081"
-)
-
 func main() {
+
 	// Logger
-	ctx := context.Background()
-	ctx, err := logger.NewLoggerContext(ctx)
-	log := logger.GetLoggerFromCtx(ctx)
+	rootCtx := context.Background()
+	rootCtx, err := logger.NewLoggerContext(rootCtx)
+	log := logger.GetLoggerFromCtx(rootCtx)
 	if err != nil {
-		log.Error(ctx, "Failed to make new logger", zap.Error(err))
+		log.Error(rootCtx, "Failed to make new logger", zap.Error(err))
 		return
 	}
 
 	// Config
 	cfg, err := config.NewCoreServerConfig()
 	if err != nil {
-		log.Error(ctx, "failed to load core configuration", zap.Error(err))
+		log.Error(rootCtx, "failed to load core configuration", zap.Error(err))
 		return
 	}
 
 	// DB Connections
 	var dbClients *db.Clients
-	pgClient, err := db.NewPostgres(ctx, cfg.Postgres)
+	pgClient, err := db.NewPostgres(rootCtx, cfg.Postgres)
 	if err != nil {
-		log.Error(ctx, "failed to connect to Postgres", zap.Error(err))
+		log.Error(rootCtx, "failed to connect to Postgres", zap.Error(err))
 		return
 	}
 	defer pgClient.Close()
@@ -54,34 +52,41 @@ func main() {
 	}
 
 	// gRPC
-	llmConnURL := fmt.Sprintf("%s:%s", llmHost, llmPort)
+	llmConnURL := fmt.Sprintf("%s:%s", cfg.LLMServer.Host, cfg.LLMServer.Port)
 	conn, err := grpc.NewClient(llmConnURL, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Error(ctx, "failed to connect to LLM Service", zap.Error(err))
+		log.Error(rootCtx, "failed to connect to LLM Service", zap.Error(err))
 		return
 	}
 	defer conn.Close()
 
 	llmClient := llm.NewLlmServiceClient(conn)
 
+	log.Info(rootCtx, "Server starting at: ",
+		zap.String("IntHost", cfg.Host),
+		zap.String("IntPort", cfg.IntPort),
+		zap.String("RestPort", cfg.RestPort))
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", cfg.RestPort))
 	if err != nil {
-		log.Error(ctx, "failed to start core-server listening", zap.Error(err))
+		log.Error(rootCtx, "failed to start core-server listening", zap.Error(err))
 		return
 	}
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptors.UnaryServerInterceptor(rootCtx)),
+	)
 
-	//TODO: add arguments to grpc.NewServer func
-	server := grpc.NewServer()
 	coreServer, err := coreserver.NewServer(llmClient, *dbClients)
 	if err != nil {
-		log.Error(ctx, "failed to create coreServer", zap.Error(err))
+		log.Error(rootCtx, "failed to create coreServer", zap.Error(err))
 	}
 
 	core.RegisterCoreServiceServer(server, coreServer)
 
-	if err = server.Serve(lis); err != nil {
-		log.Error(ctx, "Failed to launch server", zap.Error(err))
-	}
-
 	reflection.Register(server)
+
+	log.Info(rootCtx, "Server is listening")
+	if err = server.Serve(lis); err != nil {
+		log.Error(rootCtx, "Failed to launch server", zap.Error(err))
+	}
 }
