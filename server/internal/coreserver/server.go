b032d8f3c3a1ae3bc3f75a94ddb1766dba072b20
Kirill Belyaev <kirill10121415@gmail.com>
2025-11-28 21:22:54 +0300

    feat(api): add dual Errors support and HealthCheck endpoint

--- server/internal/coreserver/server.go
+++ server/internal/coreserver/server.go
@@ -4,7 +4,9 @@ package coreserver
 import (
 	"context"
 	"fmt"
-
+	"github.com/lazarevFedor/wise-task-ai/server/internal/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"time"
 
 	"go.uber.org/zap"
@@ -24,12 +26,12 @@ type Server struct {
 	postgresRepo *postgresrepository.PostgresRepository
 }
 
-func NewServer(client llm.LlmServiceClient, dbCLients db.Clients) (*Server, error) {
+func NewServer(client llm.LlmServiceClient, dbCLients db.Clients) *Server {
 	postgresRepo := postgresrepository.New(dbCLients.Postgres)
 
 	return &Server{llmClient: client,
 		postgresRepo: postgresRepo,
-	}, nil
+	}
 }
 
 func (s *Server) Prompt(ctx context.Context, req *core.PromptRequest) (*core.PromptResponse, error) {
@@ -37,39 +39,84 @@ func (s *Server) Prompt(ctx context.Context, req *core.PromptRequest) (*core.Pro
 	defer cancel()
 	log := logger.GetLoggerFromCtx(ctx)
 
-	seacrhResult, err := qdrantservice.Search(req.Text)
+	resp := &core.PromptResponse{}
+
+	searchResult, err := qdrantservice.Search(req.Text)
 	if err != nil {
-		return nil, fmt.Errorf("Prompt: failed to search in Qdrant: %w", err)
+		dualErr := errors.NewDualError(err, errors.SearchFailedErr)
+		log.Error(ctx, "prompt: failed to search in Qdrant", zap.Error(dualErr.Internal()))
+		return resp, status.Errorf(codes.Unavailable, "%s", dualErr.Public())
 	}
-	
-	log.Info(ctx, "Sending Qdrant's response to LLM...:", zap.Strings("requests", seacrhResult))
+
+	log.Debug(ctx, "Sending Qdrant's response to LLM...:", zap.Strings("requests", searchResult))
 	llmResp, err := s.llmClient.Generate(ctx, &llm.GenerateRequest{
-		Question: req.Text,
-		Contexts: seacrhResult,
+		Question:  req.Text,
+		Contexts:  searchResult,
 		RequestId: ctx.Value(logger.RequestID).(string),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("Prompt: failed to request llmClient.Generate: %w", err)
+		dualErr := errors.NewDualError(err, errors.LLMUnavailableErr)
+		log.Error(ctx, "prompt: failed to request llmClient.Generate", zap.Error(err))
+		return resp, status.Errorf(codes.Unavailable, dualErr.Public())
 	}
 
-	resp := &core.PromptResponse{Text: llmResp.Answer}
+	resp = &core.PromptResponse{
+		Text:           llmResp.Answer,
+		ProcessingTime: llmResp.ProcessingTime,
+	}
 	return resp, nil
 }
 
 func (s *Server) Feedback(ctx context.Context, req *core.FeedbackRequest) (*core.FeedbackResponse, error) {
 	log := logger.GetLoggerFromCtx(ctx)
 	log.Info(ctx, "Sending Feedback to DB...:")
-	
+
 	feedback := &entities.Feedback{
 		Request:  req.Prompt,
 		Response: req.Response,
 		Mark:     req.Mark,
 	}
+
 	resp := &core.FeedbackResponse{}
 	if err := s.postgresRepo.InsertRate(ctx, feedback); err != nil {
-		resp.Error = fmt.Sprintf("failed to insert rate to postgres db: %s", err.Error())
-		return resp, fmt.Errorf("failed to insert rate to postgres db: %w", err)
+		dualErr := errors.NewDualError(err, errors.PSQLFailedErr)
+		log.Error(ctx, "failed to insert rate to postgres db", zap.Error(err))
+		return resp, status.Errorf(codes.Unavailable, "%s", dualErr.Public())
 	}
-	resp.Error = "" 
+
+	return resp, nil
+}
+
+func (s *Server) HealthCheck(ctx context.Context, req *core.HealthRequest) (*core.HealthResponse, error) {
+	req = nil
+	log := logger.GetLoggerFromCtx(ctx)
+
+	resp := &core.HealthResponse{
+		Healthy: false,
+	}
+
+	err := qdrantservice.CheckHealth()
+	if err != nil {
+		dualErr := errors.NewDualError(err, errors.CoreUnavailableErr)
+		log.Error(ctx, "Core_HealthCheck: qdrant unhealth", zap.Error(dualErr.Internal()))
+		return resp, status.Errorf(codes.Unavailable, "%s", dualErr.Public())
+	}
+
+	llmHealthResp, err := s.llmClient.HealthCheck(ctx, &llm.HealthRequest{})
+	if err != nil {
+		dualErr := errors.NewDualError(err, errors.LLMUnavailableErr)
+		log.Error(ctx, "failed to get response from llm service", zap.Error(dualErr.Internal()))
+		return resp, status.Errorf(codes.Unavailable, "%s", dualErr.Public())
+	}
+
+	if !llmHealthResp.Healthy {
+		dualErr := errors.NewDualError(
+			fmt.Errorf("HealthCheck: LLM service is unhealth: %w", err),
+			errors.LLMUnhealthErr,
+		)
+		log.Error(ctx, "HealthCheck: LLM service is unhealth", zap.Error(dualErr.Internal()))
+		return resp, status.Errorf(codes.Unavailable, "%s", dualErr.Public())
+	}
+	resp.Healthy = true
 	return resp, nil
 }
